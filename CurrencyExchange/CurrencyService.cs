aac82d5c391210d0c0122efa1809ddbce46dde5d
JavaThugNGG <ildarjava@mail.ru>
2025-09-07 21:16:17 +0300

    feat: add CurrencyService

---
+++ CurrencyExchange/CurrencyService.cs
@@ -0,0 +1,33 @@
+﻿namespace CurrencyExchange
+{
+    public class CurrencyService
+    {
+        private readonly CurrencyDao _currencyDao;
+
+        public CurrencyService(CurrencyDao currencyDao)
+        {
+            _currencyDao = currencyDao;
+        }
+
+        public CurrencyDto GetCurrency(string code)
+        {
+            return _currencyDao.GetCurrency(code);
+        }
+
+        /*internal IList<CurrencyDto> GetAllCurrencies()
+        {
+            return _currencyDao.GetAllCurrencies();
+        }
+
+        internal CurrencyDto AddCurrency(string fullName, string code, string sign)
+        {
+            if (_currencyDao.IsCurrencyExists(code))
+            {
+                throw new ElementAlreadyExistsException("Данная валюта уже существует");
+            }
+
+            long id = _currencyDao.InsertCurrency(fullName, code, sign);
+            return new CurrencyDto(id, fullName, code, sign);
+        }*/
+    }
+}
