1a370325c94b0f2339aa5aa715678f8bf07f5e7f
JavaThugNGG <ildarjava@mail.ru>
2025-09-07 21:15:55 +0300

    feat: add CurrencyProcessor

---
+++ CurrencyExchange/CurrencyProcessor.cs
@@ -0,0 +1,10 @@
+﻿namespace CurrencyExchange
+{
+    public class CurrencyProcessor
+    {
+        public string GetCurrencyCodeWithoutSlash(string currencyCode)
+        {
+            return currencyCode.Substring(1);
+        }
+    }
+}
