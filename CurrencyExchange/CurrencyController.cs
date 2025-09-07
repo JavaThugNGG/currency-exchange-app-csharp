880c4b5169057e5c4501b0dcc0eed1f59bf40557
JavaThugNGG <ildarjava@mail.ru>
2025-09-07 21:19:52 +0300

    feat: add CurrencyController

---
+++ CurrencyExchange/CurrencyController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyExchange
+{
+    [ApiController]
+    [Route("currency")]
+    public class CurrencyController : ControllerBase
+    {
+        private readonly CurrencyService _currencyService;
+        private readonly CurrencyValidator _currencyValidator;
+        private readonly CurrencyProcessor _currencyProcessor;
+
+        public CurrencyController(CurrencyService currencyService, CurrencyValidator currencyValidator, CurrencyProcessor currencyProcessor)
+        {
+            _currencyService = currencyService;
+            _currencyValidator = currencyValidator;
+            _currencyProcessor = currencyProcessor;
+        }
+
+        [HttpGet("{code}")]//валидацию тут можно сделать, мб уберешь класс-валидатор свой
+        public IActionResult GetCurrency(string code)
+        {
+            _currencyValidator.ValidateCode(code);
+
+            CurrencyDto currency = _currencyService.GetCurrency(code);
+
+            return Ok(currency);
+        }
+    }
+}
