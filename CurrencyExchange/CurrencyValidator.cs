4f57fd1016df1a8ce0a0175d270858ee8f2742e4
JavaThugNGG <ildarjava@mail.ru>
2025-09-07 21:15:29 +0300

    feat: add CurrencyValidator

---
+++ CurrencyExchange/CurrencyValidator.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CurrencyExchange
+{
+    public class CurrencyValidator
+    {
+        public bool ValidateCode(string code)
+        {
+            return code != null && Regex.IsMatch(code, @"^[A-Za-z]{1,3}$");
+        }
+    }
+}
