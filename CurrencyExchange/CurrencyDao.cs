3c08ef400a2beb07c76bdd6c0171ddbd464529f3
JavaThugNGG <ildarjava@mail.ru>
2025-09-07 21:16:29 +0300

    feat: add CurrencyDao

---
+++ CurrencyExchange/CurrencyDao.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace CurrencyExchange
+{
+    public class CurrencyDao
+    {
+        private readonly string _connectionString;
+        public CurrencyDao(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public CurrencyDto GetCurrency(string code)
+        {
+            string query = "SELECT * FROM currencies WHERE code = @code;";
+
+            using (var conn = new SqliteConnection(_connectionString))
+            {
+                conn.Open();
+                using (var cmd = new SqliteCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@code", code);
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new CurrencyDto(
+                                Id: reader.GetInt32(reader.GetOrdinal("id")),
+                                Name: reader.GetString(reader.GetOrdinal("name")),
+                                Code: reader.GetString(reader.GetOrdinal("code")),
+                                Sign: reader.GetString(reader.GetOrdinal("sign"))
+                            );
+                        }
+                        else
+                        {
+                            throw new ElementNotFoundException("Запрашиваемая валюта не найдена");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
