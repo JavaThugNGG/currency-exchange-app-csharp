4c2fa903fb173a01f7d9395d1ff707a2d309af40
JavaThugNGG <ildarjava@mail.ru>
2025-09-08 22:41:34 +0300

    feat(mapper): add ExchangeRateMapper

---
+++ CurrencyExchange/ExchangeRateMapper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace CurrencyExchange
+{
+    public class ExchangeRateMapper
+    {
+        public ExchangeRateDto ToDto(SqliteDataReader reader)
+        {
+            long id = reader.GetInt64(reader.GetOrdinal("rateId"));
+
+            var baseCurrency = new CurrencyDto(
+                reader.GetInt64(reader.GetOrdinal("baseId")),
+                reader.GetString(reader.GetOrdinal("baseName")),
+                reader.GetString(reader.GetOrdinal("baseCode")),
+                reader.GetString(reader.GetOrdinal("baseSign"))
+            );
+
+            var targetCurrency = new CurrencyDto(
+                reader.GetInt64(reader.GetOrdinal("targetId")),
+                reader.GetString(reader.GetOrdinal("targetName")),
+                reader.GetString(reader.GetOrdinal("targetCode")),
+                reader.GetString(reader.GetOrdinal("targetSign"))
+            );
+
+            decimal rate = Convert.ToDecimal(reader.GetDouble(reader.GetOrdinal("rate")));
+
+            return new ExchangeRateDto(id, baseCurrency, targetCurrency, rate);
+        }
+    }
+}
