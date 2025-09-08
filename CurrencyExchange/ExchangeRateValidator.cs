f62ec8e6772ffdd33f435765c5cd0b2a8569a008
JavaThugNGG <ildarjava@mail.ru>
2025-09-08 22:44:15 +0300

    feat(validator): add ExchangeRateValidator

---
+++ CurrencyExchange/ExchangeRateValidator.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace CurrencyExchange
+{
+    public class ExchangeRateValidator
+    {
+        public void ValidateParameters(string baseCode, string targetCode, string rate)
+        {
+            if (string.IsNullOrEmpty(baseCode) || string.IsNullOrEmpty(targetCode) ||
+                !Regex.IsMatch(baseCode, "^[A-Za-z]{1,3}$") || !Regex.IsMatch(targetCode, "^[A-Za-z]{1,3}$"))
+            {
+                throw new ArgumentException("Отсутсвуют необходимые парамметры, либо они некорректные");
+            }
+
+            ValidateRate(rate);
+        }
+
+        public void ValidateRate(string rate)
+        {
+            if (string.IsNullOrEmpty(rate))
+            {
+                throw new ArgumentException("Параметр rate некорректный");
+            }
+
+            string normalizedRate = rate.Replace(",", ".");
+
+            if (!decimal.TryParse(normalizedRate, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out decimal result) ||
+                result <= 0)
+            {
+                throw new ArgumentException("Параметр rate некорректный");
+            }
+        }
+    }
+}
