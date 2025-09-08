8808c2bab1685a17384e0012adf5fb2b92a67985
JavaThugNGG <ildarjava@mail.ru>
2025-09-08 23:19:28 +0300

    feat(controller): add ExchangeRatesController

---
+++ CurrencyExchange/ExchangeRatesControlller.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyExchange
+{
+    [ApiController]
+    [Route("exchangeRates")]
+    public class ExchangeRatesController : ControllerBase
+    {
+        private readonly ExchangeRateService _exchangeRateService;
+        private readonly ExchangeRateValidator _exchangeRateValidator;
+        private readonly ExchangeRateProcessor _exchangeRateProcessor;
+        private readonly ExchangeProcessor _exchangeProcessor;
+
+        public ExchangeRatesController(ExchangeRateService exchangeRateService, ExchangeRateValidator exchangeRateValidator, ExchangeRateProcessor exchangeRateProcessor, ExchangeProcessor exchangeProcessor)
+        {
+            _exchangeRateService = exchangeRateService;
+            _exchangeRateValidator = exchangeRateValidator;
+            _exchangeRateProcessor = exchangeRateProcessor;
+            _exchangeProcessor = exchangeProcessor;
+        }
+
+        [HttpGet]
+        public IActionResult GetExchangeRates()
+        {
+            IList<ExchangeRateDto> exchangeRates = _exchangeRateService.GetAllRates();
+            return Ok(exchangeRates);
+        }
+
+        [HttpPost]
+        public IActionResult InsertExchangeRate([FromForm] string baseCurrencyCode, [FromForm] string targetCurrencyCode, [FromForm] string rate)
+        {
+            _exchangeRateValidator.ValidateParameters(baseCurrencyCode, targetCurrencyCode, rate);
+            _exchangeProcessor.IsSameCurrencies(baseCurrencyCode, targetCurrencyCode);
+
+            decimal rateDecimal = _exchangeRateProcessor.NormalizeRate(rate);
+
+            _exchangeRateService.AddRate(baseCurrencyCode, targetCurrencyCode, rateDecimal);
+            ExchangeRateDto exchangeRate = _exchangeRateService.GetRate(baseCurrencyCode, targetCurrencyCode);
+
+            string pairCode = baseCurrencyCode + targetCurrencyCode;
+
+            return CreatedAtRoute("GetExchangeRate",new { pairCode },exchangeRate);
+        }
+    }
+}
