7e9d8a1ec956a3414ac6d6d5296d888ede902ffb
JavaThugNGG <ildarjava@mail.ru>
2025-09-08 22:43:38 +0300

    feat(validator): add ExchangeValidator

---
+++ CurrencyExchange/ExchangeValidator.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CurrencyExchange
+{
+    public class ExchangeValidator
+    {
+        public void ValidateAmount(string amount)
+        {
+            if (string.IsNullOrEmpty(amount) || !Regex.IsMatch(amount, @"^\d{1,14}(\.\d{1,14})?$"))
+            {
+                throw new ArgumentException("Некорректное значение поля amount");
+            }
+        }
+    }
+}
