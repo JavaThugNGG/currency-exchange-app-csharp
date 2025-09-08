25e60ff2015f66c67a64165ce1a6b4801abae256
JavaThugNGG <ildarjava@mail.ru>
2025-09-08 13:49:10 +0300

    refactor(dao): add DatabaseException throwing + beatification in CurrencyDao

--- CurrencyExchange/CurrencyDao.cs
+++ CurrencyExchange/CurrencyDao.cs
@@ -5,38 +5,44 @@ namespace CurrencyExchange
     public class CurrencyDao
     {
         private readonly string _connectionString;
+
         public CurrencyDao(string connectionString)
         {
             _connectionString = connectionString;
         }
+
         public CurrencyDto GetCurrency(string code)
         {
-            string query = "SELECT * FROM currencies WHERE code = @code;";
+            var query = @"
+                SELECT *
+                FROM currencies 
+                WHERE code = @code
+            ;";
 
-            using (var conn = new SqliteConnection(_connectionString))
+            try
             {
-                conn.Open();
-                using (var cmd = new SqliteCommand(query, conn))
-                {
-                    cmd.Parameters.AddWithValue("@code", code);
+                using var connection = new SqliteConnection(_connectionString);
+                connection.Open();
 
-                    using (var reader = cmd.ExecuteReader())
-                    {
-                        if (reader.Read())
-                        {
-                            return new CurrencyDto(
-                                Id: reader.GetInt32(reader.GetOrdinal("id")),
-                                Name: reader.GetString(reader.GetOrdinal("name")),
-                                Code: reader.GetString(reader.GetOrdinal("code")),
-                                Sign: reader.GetString(reader.GetOrdinal("sign"))
-                            );
-                        }
-                        else
-                        {
-                            throw new ElementNotFoundException("Запрашиваемая валюта не найдена");
-                        }
-                    }
+                using var command = new SqliteCommand(query, connection);
+                command.Parameters.Add("@code", SqliteType.Text).Value = code;
+
+                using var reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    return new CurrencyDto(
+                        reader.GetInt32(reader.GetOrdinal("id")),
+                        reader.GetString(reader.GetOrdinal("name")),
+                        reader.GetString(reader.GetOrdinal("code")),
+                        reader.GetString(reader.GetOrdinal("sign"))
+                    );
                 }
+
+                throw new ElementNotFoundException("Запрашиваемая валюта не найдена!");
+            }
+            catch (SqliteException ex)
+            {
+                throw new DatabaseException("Ошибка при работе с БД!", ex);
             }
         }
     }
