568236d19583e6c6435d1460c3258f8150cfb908
JavaThugNGG <ildarjava@mail.ru>
2025-09-08 23:18:48 +0300

    feat(controller): add ExchangeRateController

---
+++ CurrencyExchange/ExchangeRateController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyExchange
+{
+    [ApiController]
+    [Route("exchangeRate")]
+    public class ExchangeRateController : ControllerBase
+    {
+        private readonly ExchangeRateService _exchangeRateService;
+        private readonly ExchangeRateValidator _exchangeRateValidator;
+        private readonly ExchangeRateProcessor _exchangeRateProcessor;
+
+        public ExchangeRateController(ExchangeRateService exchangeRateService, ExchangeRateValidator exchangeRateValidator, ExchangeRateProcessor exchangeRateProcessor)
+        {
+            _exchangeRateService = exchangeRateService;
+            _exchangeRateValidator = exchangeRateValidator;
+            _exchangeRateProcessor = exchangeRateProcessor;
+        }
+
+        [HttpGet("{pairCode}", Name = "GetExchangeRate") ]
+        public IActionResult GetExchangeRate(string pairCode)
+        {
+            string baseCurrencyCode = _exchangeRateProcessor.SplitBaseCurrency(pairCode);
+            string targetCurrencyCode = _exchangeRateProcessor.SplitTargetCurrency(pairCode);
+
+            ExchangeRateDto exchangeRate = _exchangeRateService.GetRate(baseCurrencyCode, targetCurrencyCode);
+            return Ok(exchangeRate);
+        }
+
+        [HttpPatch("{pairCode}")]
+        public IActionResult UpdateExchangeRate(string pairCode, [FromForm] string rate)
+        {
+
+            string baseCurrencyCode = _exchangeRateProcessor.SplitBaseCurrency(pairCode);
+            string targetCurrencyCode = _exchangeRateProcessor.SplitTargetCurrency(pairCode);
+
+            _exchangeRateValidator.ValidateRate(rate);
+
+            decimal rateDecimal = _exchangeRateProcessor.NormalizeRate(rate);
+
+            _exchangeRateService.UpdateRate(baseCurrencyCode, targetCurrencyCode, rateDecimal);
+            ExchangeRateDto updatedRate = _exchangeRateService.GetRate(baseCurrencyCode, targetCurrencyCode);
+            return Ok(updatedRate);
+        }
+    }
+}
